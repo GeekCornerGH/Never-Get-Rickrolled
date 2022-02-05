6f0bc846c316e3206205936be67642b415820776
Matteo C <35396314+Micorksen@users.noreply.github.com>
2022-02-05 19:14:07 +0100

    Update functions.js

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,5 +1,5 @@
 /*
- * Never-Get-Rickrolled functions <https://github.com/Micorksen/Never-Get-Rickrolled>
+ * Never-Get-Rickrolled <https://github.com/Micorksen/Never-Get-Rickrolled>
  * Copyright 2022 Matteo C. <https://micorksen.eu.org>
  * Available under MIT license <https://mths.be/mit>
  * Some functions come from other libraries.
