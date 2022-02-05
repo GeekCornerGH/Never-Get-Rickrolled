2b35a4172f9d156fb59b2d508fdf3e39c30df191
Matteo C <35396314+Micorksen@users.noreply.github.com>
2022-02-05 19:14:00 +0100

    Update page.js

--- src/js/page.js
+++ src/js/page.js
@@ -1,1 +1,5 @@
-
+/*
+ * Never-Get-Rickrolled <https://github.com/Micorksen/Never-Get-Rickrolled>
+ * Copyright 2022 Matteo C. <https://micorksen.eu.org>
+ * Available under MIT license <https://mths.be/mit>
+*/
