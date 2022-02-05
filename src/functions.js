6aee4481c36615b2f4a3b492d94f5db7aa88f073
Matteo C <35396314+Micorksen@users.noreply.github.com>
2022-02-05 17:05:52 +0100

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -6,24 +6,28 @@
 */
 
 "use strict";
-console.image = (url, scale = 1, position = "center") =>{
-    const image = new Image();
-    image.onload = () =>{
-        const width = image.width * scale,
-            height = image.height * scale;
-        
-        console.log(`%c.`, `
-            display: block !important;
-            margin: 10px 0px;
-            font-size: 0px;
-            padding: ${Math.floor(height / 2)}px ${Math.floor(width / 2)}px;
-            background: url(${url}) no-repeat center;
-            background-size: ${width}px ${height}px;
-            color: transparent;
-        `);
-    };
+console.image = async(url, scale = 1, position = "center") =>{
+    return new Promise((resolve) =>{
+        const image = new Image();
+        image.onload = () =>{
+            const width = image.width * scale,
+                height = image.height * scale;
 
-    image.src = url;
+            console.log(`%c.`, `
+                display: block !important;
+                margin: 10px 0px;
+                font-size: 0px;
+                padding: ${Math.floor(height / 2)}px ${Math.floor(width / 2)}px;
+                background: url(${url}) no-repeat center;
+                background-size: ${width}px ${height}px;
+                color: transparent;
+            `);
+            
+            resolve();
+        };
+
+        image.src = url;
+    });
 };
 
 console.list = (groupName, ...elements) =>{
