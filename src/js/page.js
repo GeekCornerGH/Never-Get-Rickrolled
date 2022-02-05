c8d195945cdb95234c9dc3e5505959c922eeaf29
Matteo C <35396314+Micorksen@users.noreply.github.com>
2022-02-05 19:39:50 +0100

    Update page.js

--- src/js/page.js
+++ src/js/page.js
@@ -3,3 +3,22 @@
  * Copyright 2022 Matteo C. <https://micorksen.eu.org>
  * Available under MIT license <https://mths.be/mit>
 */
+
+"use strict";
+/* Elements part. */
+const detailsButton = $("#details-button"),
+    details = $("#details");
+/* End elements part. */
+
+/* Details button part. */
+const detailsButton = $("#details-button");
+detailsButton.on("click", () =>{
+    const expanded = JSON.parse(detailsButton.attr("aria-expanded"));
+
+    detailsButton.attr("aria-expanded", !expanded);
+    details.toggleClass("hidden");
+
+    if(expanded) details.html(window.translations["open-details"]);
+    if(!expanded) details.html(window.translations["close-details"]);
+});
+/* End details button part. */
